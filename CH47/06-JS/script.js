9c4a01cad3c2feaacb5a26f41786030e51c328f1
HannMejia <hmargarita.mejiar@gmail.com>
2024-10-24 22:04:19 -0600

    Agrego carpeta CH47

---
+++ CH47/06-JS/script.js
@@ -0,0 +1,38 @@
+/*Este es un comentario
+multilínea en JavaScript*/
+//Este es un comentario de una sola línea
+/*JavaScript es un lenguaje de programación imperativo, es decir, que se lee de izquierda a derecha y de arriba hacia abajo
+Interpretado:
+Tipado debil:
+Orientado a objetos:
+ECMASCRIPT*/
+
+/*alert: palabra reservada; nos muestra un mensaje de alerta
+Se pone un punto y coma (;) después de cada instrucción*/
+//alert ("Hola mundo"); 
+
+/*console.log: imprime dentro de la consola un mensaje específico*/
+//console.log ("Este es un mensaje de la consola");
+
+/*document.write: escribe/imprime dentro de un documento*/
+//document.write("Hola, CH47");
+
+/*let: designa variables
+"nombre": para guardar datos con esa designación
+prompt: 
+"=": asigna valor*/
+//let nombre = prompt("Ingresa tu nombre");
+//alert("Hola, " + nombre + ", bienvenid@ al mundo de JavaScript");
+
+/*alert ("Inicia el cambio");
+let nombre = prompt("Regálame tu nombre");
+let signo = prompt("¿Cuál es tu signo?");
+let resultado = nombre + ", tu signo es " + signo;
+alert ("Eres " + resultado);*/
+
+alert ("Bienvenido al juego del amor <3. Da click para comenzar")
+let nombre = prompt("¿Cuál es tu nombre?");
+let amade = prompt("Ingresa el nombre de tu amad@");
+let tiempoJuntos = prompt("¿Cuántos meses llevan juntos?");
+let tiempoTotalDias = tiempoJuntos * 30
+alert (nombre + ", llevas " + tiempoTotalDias + " días junto a tu amad@ <3");
